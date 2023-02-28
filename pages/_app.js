a5f86ccf8d2c1f0b9509efb509e9a34ab7ff9b90
neutron2025 <1229507902@qq.com>
2023-02-28 18:47:14 +0800

    ceramic  example

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { Provider } from "@self.id/react";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
